e9ba25a1f8fc27dcb04a2c69eafc12f2b4df7bd6
oishigyunyu <yamamuuuura@gmail.com>
2021-08-05 14:38:31 +0000

    2.8.3 test failed

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -297,7 +297,7 @@ func (p *Parser) parseGroupExpression() ast.Expression {
 	return exp
 }
 
-func (p *Parser) parseIfExpression () ast.Expression {
+func (p *Parser) parseIfExpression() ast.Expression {
 	expression := &ast.IfExpression{Token: p.curToken}
 
 	if !p.expectPeek(token.LPAREN) {
@@ -311,5 +311,29 @@ func (p *Parser) parseIfExpression () ast.Expression {
 		return nil
 	}
 
-	if !p.exprectPeek()
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	expression.Consequence = p.parseBlockStatement()
+
+	return expression
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+
+		p.nextToken()
+
+	}
+	return block
 }
