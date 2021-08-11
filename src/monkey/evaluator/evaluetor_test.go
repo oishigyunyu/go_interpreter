e23d074e4d8961331ca4f0c9555d778fd116d96a
oishigyunyu <yamamuuuura@gmail.com>
2021-08-11 02:49:50 +0000

    boolean evaluation test passed.

--- src/monkey/evaluator/evaluetor_test.go
+++ src/monkey/evaluator/evaluetor_test.go
@@ -7,6 +7,21 @@ import (
 	"testing"
 )
 
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObjet(t, evaluated, tt.expected)
+	}
+}
+
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -38,6 +53,20 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%d, want=%d.", result.Value, expected)
+	}
+
+	return true
+}
+
+func testBooleanObjet(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T. (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("obect has wrong value. got=%t, want=%t", result.Value, expected)
+		return false
 	}
 
 	return true
