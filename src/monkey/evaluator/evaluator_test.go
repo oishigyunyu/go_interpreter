43725d69f1602034ae3815c9cf9f24da20d07ca3
oishigyunyu <yamamuuuura@gmail.com>
2021-09-09 12:14:29 +0000

    TestStringConcatenation passed

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -8,6 +8,19 @@ import (
 	"github.com/oishigyunyu/go_interpreter/parser"
 )
 
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
 func TestStringLiteral(t *testing.T) {
 	input := `"hello world!"`
 
@@ -133,6 +146,10 @@ if (10 > 1) {
 		{
 			"foobar;",
 			"identifier not found: foobar",
+		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
 		},
 	}
 
