6d55b2eaa3505ffb1455b8805b93336b8eaf4350
oishigyunyu <yamamuuuura@gmail.com>
2021-09-17 13:41:44 +0000

    parser test passed!

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -18,6 +18,7 @@ const (
 	PRUDUCT     //*
 	PREFIX      //-X or !X
 	CALL        //myFucnctoin(X)
+	Index       //arrray[index]
 )
 
 var precedences = map[token.TokenType]int{
@@ -30,6 +31,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRUDUCT,
 	token.ASTERISK: PRUDUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: Index,
 }
 
 type (
@@ -77,6 +79,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	//2つのトークンを読み込む。curTokenとpeekTokenの両方がセットされる
 	p.nextToken()
@@ -470,4 +473,17 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 
 	return list
 
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+
+	return exp
 }
